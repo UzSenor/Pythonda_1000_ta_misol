ee1b37cf3df576daabb892ca1d5e64f89dbae438
Xursand Sobirov <50676600+UzSenor@users.noreply.github.com>
2022-02-22 19:14:33 +0500

    For 40 ta misol

---
+++ For/For34.py
@@ -0,0 +1,10 @@
+# For34. n butun son berilgan. Quyidagi ketma-ketlikning dastlabki n ta hadini chiqaruvchi dastur
+# tuzing. A(1)=1 A(2)=2 A(k)=(A(k-2)+2*A(k-1))/3 k=3,4,...
+n = int(input("N="))
+A1=1
+A2=2
+for k in range(3, n+1):
+    Ak=(A1+2*A2)/3
+    A1=Ak
+    A2=Ak+1
+    print(Ak, end=" ")
