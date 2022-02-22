059524181b4082e840e37a3f05f20f1d22d7a989
Xursand Sobirov <50676600+UzSenor@users.noreply.github.com>
2022-02-22 18:55:58 +0500

    For 40 ta misol

---
+++ For/For6.py
@@ -0,0 +1,4 @@
+# For6. Bir kg konfet berilgan (haqiqiy son). 1.2,1.4-2 kg konfet narxlarini chiqaruvchi dastur tuzing.
+narx = int(input("1 kg konfet narxi:"))
+for i in range(12,21,2):
+    print(i/10,"kg = ",i/10*narx,"so'm")
